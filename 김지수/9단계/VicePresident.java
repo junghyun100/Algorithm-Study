0b2cf987d2dbfdd261d3e13083e5b09afbf2a401
rlawltn030 <rlawltn030@naver.com>
2020-02-04 21:04:02 +0900

    9단계 추가

---
+++ 김지수/9단계/VicePresident.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class VicePresident {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		int[][] arr = new int[15][15];
+		
+		for(int i = 0; i< 15; i++) {
+			for(int j = 0; j<15; j++) {
+				arr[0][i] = 1;
+			}
+		}
+		int sum = 0;
+		
+		for(int i = 0; i < T; i++) {
+			int k = sc.nextInt();
+			int n = sc.nextInt();
+			
+			
+		}
+	}
+
+}
