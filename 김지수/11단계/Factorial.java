e0d7c0d829f1bbff5dd05f6fe70a744496041552
rlawltn030 <rlawltn030@naver.com>
2020-02-14 22:10:09 +0900

    11단계 Upload

---
+++ 김지수/11단계/Factorial.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		System.out.println(factorial(N));
+		
+		sc.close();
+	}
+	public static int factorial(int n) {
+		if(n<=1){
+			return n;
+		}
+		else {
+			return factorial(n-1) * n;	
+		}
+	}
+
+}
---
+++ 김지수/11단계/Fibonacci.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		System.out.println(fibonacci(n));
+		
+		sc.close();
+	}
+	public static int fibonacci(int n) {
+		if(n == 0) {
+			return 0;
+		}else if(n == 1) {
+			return 1;
+		}
+		return fibonacci(n-1) + fibonacci(n-2);
+	}
+
+}
