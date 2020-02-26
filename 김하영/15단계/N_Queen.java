7c52365800e39601694452004264daa9d0411ffb
kimhayoung <qlalzl9@naver.com>
2020-02-26 21:12:50 +0900

    15단계 추가 upload

--- 김하영/15단계/N_Queen.java
+++ 김하영/15단계/N_Queen.java
@@ -1,38 +1,44 @@
-package Step15;
+import java.util.Scanner;
 
-import java.io.*;
-import java.util.*;
- 
 public class N_Queen {
-    static int N;
-    public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        N = scanner.nextInt();
-        check = new boolean[N][N];
+	public static int N;
+	public static int count;
 
-    }
-    
-    static boolean check[][];
-    static int result;
-    
-    private static void Queen(int x) {
-        if(x==N) {
-            result++;
-            return;
-        }
-        for(int i=0;i<N;i++) {
-//            if(!dfs(x,i)) continue;
-            check[x][i] = true;
-            Queen(x+1);
-            check[x][i] = false;
-        }
-    }
-    
-//    private static boolean dfs(int x,int y) {
-//
-//    	for(int i=x;i>=0;i--) {
-//            if(check[i][y]) return false;
-//    	}
-//        
-//    }
-//}
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		N = scanner.nextInt();
+		for (int i = 1; i <= N; i++) {
+			int[] col = new int[N + 1];
+			col[1] = i;
+			dfs(col, 1);
+		}
+
+		System.out.println(count);
+	}
+
+	public static void dfs(int[] col, int row) {
+		if (row == N) {
+			count++;
+		} else {
+			for (int i = 1; i <= N; i++) {
+				col[row + 1] = i;
+				if (isPossible(col, row + 1)) {
+					dfs(col, row + 1);
+				}
+			}
+		}
+	}
+
+	public static boolean isPossible(int[] col, int row) {
+
+		for (int i = 1; i < row; i++) {
+			if (col[i] == col[row]) {
+				return false;
+			}
+			if (Math.abs(i - row) == Math.abs(col[i] - col[row])) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
