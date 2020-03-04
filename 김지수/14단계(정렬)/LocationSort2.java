40633fa17d674fc53957bc7bc53947353611acad
rlawltn030 <rlawltn030@naver.com>
2020-03-04 17:43:35 +0900

    폴더이름 수정 및 20단계 추가

---
+++ 김지수/14단계(정렬)/LocationSort2.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class LocationSort2 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		int[][] listXY = new int[N][2];
+		for(int i = 0; i<N; i++) {
+			String[] s = br.readLine().split(" ");
+			listXY[i][0] = Integer.parseInt(s[0]);
+			listXY[i][1] = Integer.parseInt(s[1]);
+		}
+		Arrays.sort(listXY, new Comparator<int[]>(){
+			@Override
+			public int compare(int[] o1,int[] o2){
+				if(o1[1] == o2[1]){
+					return Integer.compare(o1[0], o2[0]);
+				}
+				return Integer.compare(o1[1], o2[1]);
+			}
+		});
+		
+		
+		for(int i = 0; i<N; i++) {
+			bw.write(listXY[i][0] + " " + listXY[i][1] + "\n");
+		}
+		
+		br.close();
+		bw.close();
+	}
+}
---
+++ 김지수/20단계(스택)/Zero.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Zero {
+	static int top = -1;
+	static int[] stack;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		stack = new int[N];
+		
+		for(int i = 0; i<N; i++) {
+			 push(sc.nextInt());
+		}
+		int sum = 0;
+		for(int i = 0; i<N; i++) {
+			sum+= pop();
+		}
+		System.out.println(sum);
+		sc.close();
+		
+	}
+	public static void push(int num) {
+		if(num == 0) {
+			stack[top] = 0;
+			top--;
+		}else {
+			stack[++top] = num;	
+		}
+	}
+	public static int pop() {
+		if(top == -1) {
+			return 0;
+		}else {
+			return stack[top--];
+		}
+	}
+
+}
