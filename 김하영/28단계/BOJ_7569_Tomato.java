4a0d98e72c00989a35648e4d16eb06f323f1ce9b
kimhayoung <qlalzl9@naver.com>
2020-03-19 21:59:20 +0900

    BOJ_7569_Tomato.java 오류 수정

--- 김하영/28단계/BOJ_7569_Tomato.java
+++ 김하영/28단계/BOJ_7569_Tomato.java
@@ -25,10 +25,10 @@ public class BOJ_7569_Tomato {
 				}
 			}
 		}
-		BFS(box, N, M, H);
+		BFS(box, H, N, M);
 	}
 
-	public static void BFS(int[][][] box, int N, int M, int H) {
+	public static void BFS(int[][][] box, int H, int N, int M) {
 		Queue<Point> queue = new LinkedList<Point>();
 		for (int i = 0; i < H; i++) {
 			for (int j = 0; j < N; j++) {
@@ -49,13 +49,14 @@ public class BOJ_7569_Tomato {
 				if (nextX < 0 || nextY < 0 || nextZ < 0 || nextX >= N || nextY >= M || nextZ >= H) {
 					continue;
 				}
-				if (box[nextX][nextY][nextZ] != 0) {
+				if (box[nextZ][nextX][nextY] != 0) {
 					continue;
 				}
-				box[nextX][nextY][nextZ] = box[Point.x][Point.y][Point.z] + 1;
-				queue.add(new Point(nextX, nextY, nextZ));
+				queue.add(new Point(nextZ, nextX, nextY));
+				box[nextZ][nextX][nextY] = box[Point.z][Point.x][Point.y] + 1;
 			}
 		}
+		
 		int max = 0;
 		for (int i = 0; i < H; i++) {
 			for (int j = 0; j < N; j++) {
@@ -78,7 +79,7 @@ class Point {
 	int y;
 	int z;
 
-	Point(int x, int y, int z) {
+	Point(int z, int x, int y) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
