1eb2e7dad069d276d9f484befcd60c7b7dc793b6
kimhayoung <qlalzl9@naver.com>
2020-03-26 21:04:12 +0900

    16단계 추가 upload

---
+++ 김하영/16단계/BOJ_1904_01tile.java
@@ -0,0 +1,23 @@
+package Step16;
+
+import java.util.Scanner;
+
+public class BOJ_1904_01tile {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int N = scanner.nextInt();
+		long P[] = new long[N + 1];
+		P[0] = 0;
+		P[1] = 1;
+		P[2] = 2;
+
+		for (int i = 3; i <= N; i++) {
+			P[i] = P[i - 2] + P[i - 1];
+		}
+		for(int i = 0; i<P.length; i++) {
+			System.out.println(P[i]);
+		}
+		System.out.println(P[N] % 15746);
+	}
+}
